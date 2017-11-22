4c2c9a055cf19b71c39f89933a9150ed9c0f9085
Lukas Hechenberger <admin@ls-age.com>
2017-11-22 15:02:59 +0100

    chore: Project setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import recommendedBump from 'conventional-recommended-bump';
+import config from './lib/conventional-bump-config';
+
+export default function bump() {
+  return Promise.reject(new Error('Not implemented'))
+}
