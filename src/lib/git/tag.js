411d1ce529bf1def1cf314b36ff839b96b747f66
Lukas Hechenberger <admin@ls-age.com>
2017-12-16 17:30:14 +0100

    style(eslint): Fix max line length

--- src/lib/git/tag.js
+++ src/lib/git/tag.js
@@ -11,7 +11,10 @@ export function fetchTags({ cwd }) {
 }
 
 export function getTags({ cwd }) {
-  return run('git', ['for-each-ref', '--sort', 'creatordate', '--format', '{ "name": "%(refname:short)", "date": "%(creatordate)" }', 'refs/tags'], { cwd })
+  return run('git', ['for-each-ref', '--sort', 'creatordate',
+    '--format', '{ "name": "%(refname:short)", "date": "%(creatordate)" }',
+    'refs/tags',
+  ], { cwd })
     .then(({ stdout }) => stdout.split('\n').filter(l => l.length)
       .map(JSON.parse)
       .reverse()
