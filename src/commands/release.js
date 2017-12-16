a7b24c0663737b5b3e84d0296dc0fa903abfaca8
Lukas Hechenberger <admin@ls-age.com>
2017-12-16 20:16:54 +0100

    fix: Use correct prerelease option

--- src/commands/release.js
+++ src/commands/release.js
@@ -112,7 +112,7 @@ export async function createRelease(options) {
     repo,
     tag_name: `${tagPrefix}${bump.version}`,
     body: releaseBody,
-    prerelease: releaseBranch !== true && releaseBranch,
+    prerelease: releaseBranch !== true,
   });
 
   return bump.version;
