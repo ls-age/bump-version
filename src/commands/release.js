57cee0429a34c87643e17659e8850bdba9afa6e6
Lukas Hechenberger <admin@ls-age.com>
2017-12-16 13:20:54 +0100

    style: Check if cwd is clean first

--- src/commands/release.js
+++ src/commands/release.js
@@ -1,7 +1,12 @@
 import { Command } from '@ls-age/expose';
+import isClean from '../lib/git/status';
 import { onReleaseBranch } from './on-release-branch';
 
 export async function createRelease(options) {
+  if (!(await isClean(options))) {
+    throw new Error('Working directory has uncommitted changes');
+  }
+
   const releaseBranch = await onReleaseBranch(options);
 
   if (!releaseBranch) {
