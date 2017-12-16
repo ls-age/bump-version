c8196c8250ab3e25307afe980d29591bd9426bbf
Lukas Hechenberger <admin@ls-age.com>
2017-12-16 11:17:36 +0100

    style(eslint): Remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import recommendedBump from 'conventional-recommended-bump';
-import config from './lib/conventional-bump-config';
 
 export default function bump() {
   return Promise.reject(new Error('Not implemented'))
