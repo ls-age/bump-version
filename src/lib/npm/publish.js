4390bcba214725e920f8fee2e2ee8336c65d06d3
LukasHechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2020-08-03 08:53:47 +0200

    fix: Prevent git checks for pnpm

--- src/lib/npm/publish.js
+++ src/lib/npm/publish.js
@@ -2,7 +2,13 @@ import { join } from 'path';
 import run from '../run';
 
 export default function publishToNpm({ packageManager = 'npm', cwd, dir, tag }) {
-  return run(packageManager, ['publish', ...(tag ? ['--tag', tag] : []), '--access', 'public'], {
+  const args = ['publish', ...(tag ? ['--tag', tag] : []), '--access', 'public'];
+
+  if (packageManager === 'pnpm') {
+    args.push('--no-git-checks');
+  }
+
+  return run(packageManager, args, {
     cwd: dir ? join(cwd || process.cwd(), dir) : cwd,
   });
 }
